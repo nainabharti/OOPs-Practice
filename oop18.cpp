4f82469c72ad41eb34ed47bf8b3bdc34a0fa83f8
Naina Bharti <nainabharti2005@gmail.com>
2026-09-25 22:32:13 +0530

    Added Abstraction , inline function and default argument concept

--- oop18.cpp
+++ oop18.cpp
@@ -1,5 +1,6 @@
-//abstractionexample
+//abstraction example
 #include<iostream>
+#include<conio.h>
 using namespace std;
 /*
 class AbstractionExample{
@@ -24,6 +25,7 @@ int main()
 }
 */
 //inline function
+/*
  inline float mul(float a, float b)
 {
     return (a*b);
@@ -38,5 +40,35 @@ int main(){
 
     cout<<mul(a,b)<<"\n";
     cout<<div(a,b)<<"\n";
+    return 0;
+}
+*/
+//default argument
+    float value(float p, int n, float r=0.15){
+        int i=1;
+        int year = 1;
+        float sum = p;
+
+        while(i<=n){
+            sum = sum*(1+r);
+            year =year+1;
+            i++;
+        }
+        return(sum);
+    }
+    void printline(char ch = '*', int len =40){
+        for(int i=1;i<=len;i++){
+            cout<<ch<<" ";
+        }
+    }
+    int main(){
+    float amount;
+    printline();
+    amount = value(5000.05,5);
+    cout<<"\n Final Value = "<<amount<<"\n\n";
+    amount =value(10000.00,5,0.30);
+    cout<<"\n Final Value = "<<amount<<"\n\n";
+    printline('=');
+    getch();
     return 0;
 }
